5e58f0ed909e354b15f44db991ad834eb6d1f5a7
Dmitri <dimamoryk@gmail.com>
2026-03-24 18:44:34 +0300

    modified

--- src/main/java/config/ConfigConstants.java
+++ src/main/java/config/ConfigConstants.java
@@ -3,7 +3,7 @@ package config;
 public class ConfigConstants {
 
     public static final String BASE_URL =
-            System.getProperty("base.url", "https://petstore.swagger.io/#");
+            System.getProperty("base.url", "https://wiremock:8080");
 
     public static final String SCHEMA_PATH = "/schema/CreateUser.json";
 }
